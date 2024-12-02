1847efc4bfddfe0c58d8706d7900759bf0690f58
Ekya1411 <142556703+Ekya1411@users.noreply.github.com>
2024-12-02 10:57:29 +0700

    add : dashboard admin

---
+++ Tata_Tertib/assets/sidebar.php
@@ -0,0 +1,11 @@
+<div id="sidebar" class="sidebar">
+    <div class="sidebar-logo">
+        <img src="../../assets/icon/logo_polinema.png" alt="LogoPolinema">
+        <p>Politeknik Negeri Malang</p>
+    </div>
+    <ul class="sidebar-menu">
+        <li><img src="../../assets/icon/house-icon.svg" alt=""><a href="#"><i class="bi bi-house"></i> <span>Home</span></a></li>
+        <li><img src="../../assets/icon/warning-icon.svg" alt=""><a href="#"><i class="bi bi-info-circle"></i> <span>About</span></a></li>
+        <li><img src="../../assets/icon/history-icon.svg" alt=""><a href="#"><i class="bi bi-gear"></i> <span>Settings</span></a></li>
+    </ul>
+</div>
