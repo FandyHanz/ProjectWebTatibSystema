d7d29e8a63559bb830cd3cbf98163c22246803da
FandyHanz <hanzyura28@gmail.com>
2024-12-08 17:50:02 +0700

    Add: model tendik and model mahasiswa

---
+++ Tata_Tertib/models/Tendik.php
@@ -0,0 +1,19 @@
+<?php
+include_once 'core/koneksi.php';
+Class Tendik extends Koneksi{
+    public function __construct(){
+        parent :: __construct();
+    }
+    public function getTabelpelanggaranTendik(){
+        $sql = "SELECT * FROM pelanggaran_tendik";
+        $result = $this -> db -> query($sql);
+        return $result;
+    }
+
+    public function getUserTendik(){
+        $sql = "SELECT * FROM Karyawan";
+        $result = $this -> db -> query($sql);
+        return $result;
+    }
+}
+?>
