62b9704109b9c5752e76d5ec5d0b363f32e60300
Sadiya8924 <sadiyamaritza8924@gmail.com>
2024-12-09 11:38:40 +0700

    add : mhs dashboard ui

---
+++ Tata_Tertib/views/dashboard/mhs/dashboard-admin.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website with Sidebar</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+
+<body>
+    <!-- Sidebar -->
+    <?php include_once '../../../assets/sidebar.php'; ?>
+
+    <!-- Main Content -->
+    <div class="main-content">
+        <!-- Header -->
+        <?php include '../../../assets/header.php'; ?>
+
+        <!-- Content -->
+        <div class="table-container">
+            <h1>List Pelanggaran Mahasiswa</h1>
+
+            <!-- toggle button untuk navigasi table -->
+            <div class="table-nav" data-toggle="buttons">
+                <ul id="tab-nav">
+                    <li class="nav-table-button active"><a href="#Mahasiswa"><img src="../../../assets/icon/student-icon.svg" class="icon" style="opacity: 1;" alt="">Pelanggaran Pribadi</a></li>
+                </ul>
+            </div>
+            <div class="line">
+                <div class="line-active" id="line-active"></div>
+            </div>
+
+            <!-- Tempat untuk memuat konten tabel -->
+            <div id="table-content-container" class="table-content-container">
+                <!-- Konten tabel akan dimuat di sini -->
+            </div>
+        </div>
+
+        <!-- Footer -->
+        <?php include '../../../assets/footer.php'; ?>
+    </div>
+
+    <script type="module">
+        // Mengimpor dan menjalankan fungsi dari tabs.js
+        import {
+            initializeTabs
+        } from './tabs.js';
+        document.addEventListener('DOMContentLoaded', () => {
+            initializeTabs(); // Menjalankan fungsi untuk menginisialisasi tab
+        });
+    </script>
+    <script src="script.js"></script>
+</body>
+
+</html>
