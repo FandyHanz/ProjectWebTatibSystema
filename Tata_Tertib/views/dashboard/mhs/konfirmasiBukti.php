f855038c04086f861bd7fd152db978f1e68c29a3
Sadiya8924 <sadiyamaritza8924@gmail.com>
2024-12-10 09:48:14 +0700

    merubah teks

--- Tata_Tertib/views/dashboard/mhs/konfirmasiBukti.php
+++ Tata_Tertib/views/dashboard/mhs/konfirmasiBukti.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Konfirmasi Bukti dengan Modal Bootstrap</title>
+    <title>Konfirmasi Bukti</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -63,7 +63,7 @@
         </div>
     </div>
 
-    <!-- Bootstrap JS Bundle with Popper -->
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 
     <!-- Script -->
--- Tata_Tertib/views/dashboard/mhs/lampiran.php
+++ Tata_Tertib/views/dashboard/mhs/lampiran.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Popup Lampiran dengan Bootstrap</title>
+    <title>Popup Lampiran</title>
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
@@ -42,7 +42,7 @@
         </div>
     </div>
 
-    <!-- Bootstrap JS Bundle with Popper -->
+    <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
