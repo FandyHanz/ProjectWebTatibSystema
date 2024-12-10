af4ce23d699a819f3667ec3895d88e6d80a1d3d4
aslamrosul <nvrsrndrmrx@gmail.com>
2024-12-10 07:18:51 +0700

    perbaikan arah ke script js

--- Tata_Tertib/views/dashboard/dashboard_dpa/dashboard.php
+++ Tata_Tertib/views/dashboard/dashboard_dpa/dashboard.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Website with Sidebar</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="views/dashboard/tendik/style.css">
+    
     <link rel="stylesheet" href="style.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
@@ -177,13 +177,13 @@
 
     <!-- Scripts -->
     <script type="module">
-        import { initializeTabs } from '../views/dashboard/tabs.js';
+        import { initializeTabs } from './tabs.js';
 
         document.addEventListener('DOMContentLoaded', () => {
             initializeTabs();
         });
     </script>
-    <script src="../views/dashboard/script.js"></script>
+    <script src="script.js"></script>
 </body>
 
 </html>
